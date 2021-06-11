3f9c3605dc7e3ef7218a33d15c4e8df795f50413
Jamithsheron <jamithsheron5@gmail.com>
2021-06-11 18:52:28 +0530

    create class for calculate and display scores

---
+++ snake_game/src/snake_game/score_board.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snake_game;
+
+import java.awt.Color;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Jamit
+ */
+public class score_board extends JPanel{
+    score_board(){
+        setBounds(0, 584, 584, 77);
+        setBackground(Color.white);
+    }
+}
