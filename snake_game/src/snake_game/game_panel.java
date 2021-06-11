f2e9232dc66366d4dd85f4b300be3c8017116446
Jamithsheron <jamithsheron5@gmail.com>
2021-06-11 18:51:13 +0530

    set sizes and color

---
+++ snake_game/src/snake_game/game_panel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snake_game;
+
+import java.awt.Color;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Jamit
+ */
+public class game_panel extends JPanel {
+
+    public game_panel() {
+        setBounds(0, 0, 584, 584);
+        setBackground(Color.red);
+    }
+    
+}
