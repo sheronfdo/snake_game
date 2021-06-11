82b53a21b0e56773c0ecfa15d2b41b21546bc4b4
Jamithsheron <jamithsheron5@gmail.com>
2021-06-11 17:50:58 +0530

    create main_frame class for jframe extends jframe libraries and set sizes and visible

---
+++ snake_game/src/snake_game/main_frame.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snake_game;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Jamit
+ */
+public class main_frame extends JFrame{
+    main_frame(){
+        this.setBounds(1500, 0, 600, 600);
+        this.setVisible(true);
+    }
+    
+}
