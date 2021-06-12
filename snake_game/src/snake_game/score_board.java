49f2abb68566d02ec771b4ed20ecacb498ab604b
Jamithsheron <jamithsheron5@gmail.com>
2021-06-13 01:48:28 +0530

    (score board ) all funtions are completed

--- snake_game/src/snake_game/score_board.java
+++ snake_game/src/snake_game/score_board.java
@@ -6,9 +6,13 @@
 package snake_game;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import static snake_game.game_panel.unit_size;
+import static snake_game.game_panel.screen_height;
+import static snake_game.game_panel.screen_width;
 
 /**
  *
@@ -16,17 +20,20 @@ import static snake_game.game_panel.unit_size;
  */
 public class score_board extends JPanel{
     //constructor of class 
+    static int eatenapples;
+    JLabel scores;
     score_board(){
+        scores = new JLabel("scores");
+        //scores.setBounds(25, 25, 100, 50);
+        scores.setFont(new Font("Ink Free",Font.BOLD, 60));
+        //scores.setBackground(Color.red);
+        scores.setOpaque(true);
         this.setBounds(0, game_panel.screen_height, game_panel.screen_width, 100);
-        this.setBackground(Color.green);
+        this.add(scores);
+        //this.setBackground(Color.green);
     }
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g); // get super access for this graphics
-        draw(g); // call and pass graphic object to method draw
-    }
-
-    public void draw(Graphics g) {
-    
+    public void setscore(int i){
+        eatenapples = i;
+        scores.setText("SCORES : "+Integer.toString(eatenapples*5));
     }
 }
