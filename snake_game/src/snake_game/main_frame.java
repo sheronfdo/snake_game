91f0d6040e02a807955d375d3bc57baac070e480
Jamithsheron <jamithsheron5@gmail.com>
2021-06-13 02:34:49 +0530

    make some chamges (set relative location as comment) put comments for lines to identify code lines

--- snake_game/src/snake_game/main_frame.java
+++ snake_game/src/snake_game/main_frame.java
@@ -13,19 +13,19 @@ import javax.swing.JFrame;
 public class main_frame extends JFrame{
     //constructor of class 
     main_frame(){
-        game_panel game = new game_panel();
-        score_board score = game.score;
+        game_panel game = new game_panel(); //create object for game panel
+        score_board score = game.score; // create object score_board type but initialized score object of score_board type in created game_panel class
         
-        this.setResizable(false);
-        this.setTitle("Cobra");
+        this.setResizable(false); // set resizability
+        this.setTitle("Cobra"); // set title for programme
         this.setLayout(null);//create layout as null
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//set close operator
-        this.setBounds(0, 0, 616, 739);//set frame visible location and size
+        this.setBounds(1500, 0, 616, 739);//set frame visible location and size
         this.add(game);// add game panel to frame
         this.add(score);// add score board to frame
         this.setVisible(true);// set visibility 
         //this.pack();
-        this.setLocationRelativeTo(null);
+        //this.setLocationRelativeTo(null);
         
     }
     
