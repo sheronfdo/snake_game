11dbdcd313bb38ddd9006bf21b43ff1044c4b604
Jamithsheron <jamithsheron5@gmail.com>
2021-06-12 19:31:54 +0530

    connect score board through game panel

--- snake_game/src/snake_game/main_frame.java
+++ snake_game/src/snake_game/main_frame.java
@@ -14,7 +14,7 @@ public class main_frame extends JFrame{
     //constructor of class 
     main_frame(){
         game_panel game = new game_panel();
-        
+        score_board score = game.score;
         
         this.setResizable(false);
         this.setTitle("Cobra");
@@ -22,7 +22,7 @@ public class main_frame extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//set close operator
         this.setBounds(0, 0, 616, 739);//set frame visible location and size
         this.add(game);// add game panel to frame
-        this.add(new score_board());// add score board to frame
+        this.add(score);// add score board to frame
         this.setVisible(true);// set visibility 
         //this.pack();
         this.setLocationRelativeTo(null);
