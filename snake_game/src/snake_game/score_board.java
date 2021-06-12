157ddad7a7128f724fdd4f767b8aa401688751c6
Jamithsheron <jamithsheron5@gmail.com>
2021-06-12 19:34:11 +0530

    create paintcomponent override for jpanel and create draw method

--- snake_game/src/snake_game/score_board.java
+++ snake_game/src/snake_game/score_board.java
@@ -6,7 +6,9 @@
 package snake_game;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import javax.swing.JPanel;
+import static snake_game.game_panel.unit_size;
 
 /**
  *
@@ -17,5 +19,14 @@ public class score_board extends JPanel{
     score_board(){
         this.setBounds(0, game_panel.screen_height, game_panel.screen_width, 100);
         this.setBackground(Color.green);
+    }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g); // get super access for this graphics
+        draw(g); // call and pass graphic object to method draw
+    }
+
+    public void draw(Graphics g) {
+    
     }
 }
